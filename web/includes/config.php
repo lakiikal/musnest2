28d3e1bceef5d691bf4c99e8ad71e57d4735e736
Laki <igorlakic79@gmail.com>
2019-10-20 15:00:02 +0200

    changed config.php

--- web/includes/config.php
+++ web/includes/config.php
@@ -4,7 +4,7 @@
 
 	$timezone = date_default_timezone_set("Europe/Belgrade");
 
-	$con = mysqli_connect("localhost", "root", "", "musnest");
+	$con = mysqli_connect("remotemysql.com", "rlwjOzs3kq", "O4Adwg0qZX", "rlwjOzs3kq");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
