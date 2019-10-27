58b35d213f0a1eb180e6eaf7edc6b1f389bddc79
lakiikal <igorlakic79@gmail.com>
2019-10-27 10:22:21 +0100

    bootstrap

--- web/album.php
+++ web/album.php
@@ -39,7 +39,7 @@ $artist = $album->getArtist();
 			$albumSong = new Song($con, $songId);
 			$albumArtist = $albumSong->getArtist();
 
-			echo 	"<li id='fake' class='tracklistRow' style='height: 1px; padding: 0px;'></li>
+			echo 	"<li id='fake' class='tracklistRow' style='height: 50px; padding: 0px;'></li>
 					<li class='tracklistRow'>
 						<div class='trackCount'>
 							<img class='play' src='assets/images/icons/play-white.png' onclick='setTrack(\"" . $albumSong->getId() . "\", tempPlaylist, true)'>
